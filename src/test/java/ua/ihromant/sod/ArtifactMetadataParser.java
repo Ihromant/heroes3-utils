9ad3243ccf376da4960592894cd8687b319f7802
Ihromant <vespertilion@gmail.com>
2020-12-06 15:59:12 +0200

    Added artifact downloader

--- src/test/java/ua/ihromant/sod/ArtifactMetadataParser.java
+++ src/test/java/ua/ihromant/sod/ArtifactMetadataParser.java
@@ -5,7 +5,9 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.junit.jupiter.api.Test;
 
+import javax.imageio.ImageIO;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
@@ -18,11 +20,12 @@ public class ArtifactMetadataParser {
     public void parse() throws IOException {
         Document doc = Jsoup.parse(new URL("https://heroes.thelazy.net/index.php/List_of_artifacts"), 100000);
         System.out.println(doc.toString());
-        Elements table = doc.getElementsByClass("sortable").get(0).children().get(1).children();
+        Elements table = doc.getElementsByClass("sortable").get(0).child(1).children();
         System.out.println(table.size());
         for (int i = 1; i < table.size(); i++) {
             Elements tds = table.get(i).children();
             String name = tds.get(0).text().replace("'", "").replace(' ', '_').toUpperCase();
+            ImageIO.write(ImageIO.read(new URL("https://heroes.thelazy.net" + tds.get(0).child(0).child(0).attr("src"))), "png", new File("/home/ihromant/artifacts/" + name.toLowerCase() + ".png"));
             String stVal = tds.get(1).text();
             boolean combo = "Combo".equalsIgnoreCase(stVal);
             SlotType sType = combo ? null : SlotType.valueOf(stVal.toUpperCase());
