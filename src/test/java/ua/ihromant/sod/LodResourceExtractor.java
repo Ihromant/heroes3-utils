d023609354bfe76eb8d833824a022185e8d18b8c
Ihromant <vespertilion@gmail.com>
2020-12-10 01:02:49 +0200

    Added def extracting

--- src/test/java/ua/ihromant/sod/LodResourceExtractor.java
+++ src/test/java/ua/ihromant/sod/LodResourceExtractor.java
@@ -3,28 +3,31 @@ package ua.ihromant.sod;
 import com.google.common.io.LittleEndianDataInputStream;
 import lombok.Getter;
 import lombok.Setter;
-import org.apache.commons.imaging.ImageReadException;
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.zip.DataFormatException;
 import java.util.zip.Inflater;
 
 public class LodResourceExtractor {
     private static final String baseFolder = "/tmp/images/";
-    private static final String lodPosition = "/home/ihromant/Games/Heroes III/Heroes III Complete/Data/H3bitmap.lod";
+    private static final String lodPosition = "/home/ihromant/Games/Heroes III/Heroes III Complete/Data/H3sprite.lod";
     @Test
-    public void extractFromLod() throws IOException, DataFormatException, ImageReadException {
+    public void extractFromLod() throws IOException, DataFormatException {
         RandomAccessFile file = new RandomAccessFile(new File(lodPosition), "r");
         byte[] header = new byte[4];
         file.read(header);
@@ -44,7 +47,7 @@ public class LodResourceExtractor {
             }
             FileMetadata fileMeta = new FileMetadata();
             String n = new String(name);
-            n = n.substring(0, n.indexOf((char) 0));
+            n = n.substring(0, n.indexOf((char) 0)).toLowerCase();
             fileMeta.setName(n);
             fileMeta.setOffset(readInt(file));
             fileMeta.setSize(readInt(file));
@@ -61,50 +64,198 @@ public class LodResourceExtractor {
             if (isPcx(bytes)) {
                 readPcx(bytes, fileName);
             } else {
-                IOUtils.write(bytes, new FileOutputStream(fileName));
+                String extension = fileName.substring(fileName.indexOf('.') + 1);
+                if ("def".equals(extension)) {
+                    if ("sgtwmta.def".equals(meta.getName()) || "sgtwmtb.def".equals(meta.getName())) {
+                        continue;
+                    }
+                    readDef(bytes, meta.getName());
+                } else {
+                    IOUtils.write(bytes, new FileOutputStream(fileName));
+                }
             }
         }
         files.forEach(f -> System.out.println(f.getName() + " " + f.getOffset() + " " + f.getSize() + " " + f.getCSize()));
     }
 
+    private void readDef(byte[] bytes, String fileName) throws IOException {
+        String folderName = fileName.substring(0, fileName.indexOf("."));
+        File dir = new File(baseFolder, folderName);
+        dir.mkdir();
+        ByteWrapper str = new ByteWrapper(bytes);
+        int t = str.readInt();
+        str.readInt();
+        str.readInt();
+        int blocks = str.readInt();
+        int[] palette = new int[256];
+        for (int i = 0; i < 256; i++) {
+            palette[i] = str.readRGB();
+        }
+        Map<Integer, List<Integer>> offsets = new HashMap<>();
+        for (int i = 0; i < blocks; i++) {
+            int blockId = str.readInt();
+            List<Integer> list = new ArrayList<>();
+            offsets.put(blockId, list);
+            int entries = str.readInt();
+            str.readInt();
+            str.readInt();
+            for (int j = 0; j < entries; j++) {
+                String name = str.readString(13);
+                System.out.println(name);
+            }
+            for (int j = 0; j < entries; j++) {
+                list.add(str.readInt());
+            }
+        }
+        for (Map.Entry<Integer, List<Integer>> e : offsets.entrySet()) {
+            for (int j = 0; j < e.getValue().size(); j++) {
+                int offs = e.getValue().get(j);
+                str.seek(offs);
+                str.readInt();
+                int fmt = str.readInt();
+                int fw = str.readInt();
+                int fh = str.readInt();
+                int w = str.readInt();
+                int h = str.readInt();
+                int lm = str.readInt();
+                int tm = str.readInt();
+                if (w != 0 && h != 0) {
+                    ByteArrayOutputStream pixelData = new ByteArrayOutputStream();
+                    switch (fmt) {
+                        case 0:
+                            pixelData.write(str.readBytes(w * h));
+                            break;
+                        case 1:
+                            int[] lineOffs = new int[h];
+                            for (int i = 0; i < h; i++) {
+                                lineOffs[i] = str.readInt();
+                            }
+                            for (int lineOff : lineOffs) {
+                                str.seek(offs + 32 + lineOff);
+                                int totalRowLength = 0;
+                                while (totalRowLength < w) {
+                                    int code = str.readUnsignedByte();
+                                    int length = str.readUnsignedByte() + 1;
+                                    if (code == 0xff) {
+                                        pixelData.write(str.readBytes(length));
+                                    } else {
+                                        byte[] res = new byte[length];
+                                        Arrays.fill(res, (byte) code);
+                                        pixelData.write(res);
+                                    }
+                                    totalRowLength = totalRowLength + length;
+                                }
+                            }
+                            break;
+                        case 2:
+                            lineOffs = new int[h];
+                            for (int i = 0; i < h; i++) {
+                                lineOffs[i] = str.readUnsignedShort();
+                            }
+                            str.readUnsignedShort();
+                            for (int lineOff : lineOffs) {
+                                str.seek(offs + 32 + lineOff);
+                                int totalRowLength = 0;
+                                while (totalRowLength < w) {
+                                    int segment = str.readUnsignedByte();
+                                    int code = segment >> 5;
+                                    int length = (segment&0x1f) + 1;
+                                    if (code == 7) {
+                                        pixelData.write(str.readBytes(length));
+                                    } else {
+                                        byte[] res = new byte[length];
+                                        Arrays.fill(res, (byte) code);
+                                        pixelData.write(res);
+                                    }
+                                    totalRowLength = totalRowLength + length;
+                                }
+                            }
+                            break;
+                        case 3:
+                            int[][] lineoffs = new int[h][w / 32];
+                            for (int i = 0; i < h; i++) {
+                                for (int k = 0; k < w / 32; k++) {
+                                    lineoffs[i][k] = str.readUnsignedShort();
+                                }
+                            }
+                            for (int[] lineoff : lineoffs) {
+                                for (int i : lineoff) {
+                                    str.seek(offs + 32 + i);
+                                    int totalBlockLength = 0;
+                                    while (totalBlockLength < 32) {
+                                        int segment = str.readUnsignedByte();
+                                        int code = segment >> 5;
+                                        int length = (segment&0x1f) + 1;
+                                        if (code == 7) {
+                                            pixelData.write(str.readBytes(length));
+                                        } else {
+                                            byte[] res = new byte[length];
+                                            Arrays.fill(res, (byte) code);
+                                            pixelData.write(res);
+                                        }
+                                        totalBlockLength = totalBlockLength + length;
+                                    }
+                                }
+                            }
+                            break;
+                        default:
+                            throw new IllegalStateException();
+                    }
+                    BufferedImage img = readPalettedImage(w, h, palette, new ByteWrapper(pixelData.toByteArray()));
+                    File result = new File(dir, e.getKey() + "_" + j + ".png");
+                    ImageIO.write(img, "png", result);
+                }
+            }
+        }
+    }
+
     private int readInt(RandomAccessFile file) throws IOException {
         byte[] bytes = new byte[4];
         file.read(bytes);
-        return new LittleEndianDataInputStream(new ByteArrayInputStream(bytes)).readInt();
+        return new ByteWrapper(bytes).readInt();
     }
 
-    private void readPcx(byte[] bytes, String fileName) throws IOException, ImageReadException {
-        LittleEndianDataInputStream stream = new LittleEndianDataInputStream(new ByteArrayInputStream(bytes));
+    private void readPcx(byte[] bytes, String fileName) throws IOException {
+        ByteWrapper stream = new ByteWrapper(bytes);
         int size = stream.readInt();
         int width = stream.readInt();
         int height = stream.readInt();
-        BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+        BufferedImage img;
         if (size == width * height) {
             int[] palette = new int[256];
             for (int i = 12 + size; i < 12 + size + 3 * 256; i = i + 3) {
                 palette[(i - 12 - size) / 3] = (Byte.toUnsignedInt(bytes[i]) << 16) | (Byte.toUnsignedInt(bytes[i + 1]) << 8) | (Byte.toUnsignedInt(bytes[i + 2]));
             }
-            for (int i = 0; i < height; i++) {
-                for (int j = 0; j < width; j++) {
-                    int idx = stream.readUnsignedByte();
-                    img.setRGB(j, i, palette[idx]);
-                }
-            }
+            img = readPalettedImage(width, height, palette, stream);
         } else {
-            for (int i = 0; i < height; i++) {
-                for (int j = 0; j < width; j++) {
-                    int r = stream.readUnsignedByte() << 16;
-                    int g = stream.readUnsignedByte() << 8;
-                    int b = stream.readUnsignedByte();
-                    img.setRGB(j, i, r | g | b);
-                }
+            img = readRGBImage(width, height, stream);
+        }
+        ImageIO.write(img, "bmp", new File(fileName.replace("pcx", "bmp")));
+    }
+
+    private BufferedImage readPalettedImage(int width, int height, int[] palette, ByteWrapper stream) throws IOException {
+        BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                int idx = stream.readUnsignedByte();
+                img.setRGB(j, i, palette[idx]);
             }
         }
-        ImageIO.write(img, "bmp", new File(fileName.toLowerCase().replace("pcx", "bmp")));
+        return img;
+    }
+
+    private BufferedImage readRGBImage(int width, int height, ByteWrapper stream) throws IOException {
+        BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                img.setRGB(j, i, stream.readRGB());
+            }
+        }
+        return img;
     }
 
     private boolean isPcx(byte[] array) throws IOException {
-        LittleEndianDataInputStream stream = new LittleEndianDataInputStream(new ByteArrayInputStream(array));
+        ByteWrapper stream = new ByteWrapper(array);
         int size = stream.readInt();
         int width = stream.readInt();
         int height = stream.readInt();
@@ -125,6 +276,53 @@ public class LodResourceExtractor {
         byte[] result = new byte[size];
         file.read(result);
         return result;
+    }
+
+    public static class ByteWrapper {
+        private final LittleEndianDataInputStream str;
+
+        public ByteWrapper(byte[] bytes) {
+            this.str = new LittleEndianDataInputStream(new ByteArrayInputStream(bytes));
+        }
+
+        public void seek(int position) throws IOException {
+            str.reset();
+            str.skipBytes(position);
+        }
+
+        public int readInt() throws IOException {
+            return str.readInt();
+        }
+
+        public String readString(int characters) throws IOException {
+            char[] nm = new char[characters];
+            for (int k = 0; k < characters; k++) {
+                nm[k] = (char) str.readByte();
+            }
+            return new String(nm);
+        }
+
+        public int readUnsignedByte() throws IOException {
+            return str.readUnsignedByte();
+        }
+
+        public int readRGB() throws IOException {
+            return (str.readUnsignedByte() << 16) | (str.readUnsignedByte() << 8) | str.readUnsignedByte();
+        }
+
+        public byte[] readBytes(int size) throws IOException {
+            byte[] bytes = new byte[size];
+            str.read(bytes);
+            return bytes;
+        }
+
+        public byte readByte() throws IOException {
+            return str.readByte();
+        }
+
+        public int readUnsignedShort() throws IOException {
+            return str.readUnsignedShort();
+        }
     }
 
     @Setter
