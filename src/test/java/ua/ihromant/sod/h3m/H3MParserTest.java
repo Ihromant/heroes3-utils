5be53596a1c91912527ed6c6c6631bb478e00ac2
ihromant <vespertilion@gmail.com>
2022-05-21 19:32:28 +0300

    Added rivers, roads and grid

--- src/test/java/ua/ihromant/sod/h3m/H3MParserTest.java
+++ src/test/java/ua/ihromant/sod/h3m/H3MParserTest.java
@@ -10,13 +10,10 @@ import org.junit.jupiter.api.Test;
 import ua.ihromant.sod.utils.H3MParser;
 import ua.ihromant.sod.utils.entities.MapMetadata;
 import ua.ihromant.sod.utils.entities.MapTile;
+import ua.ihromant.sod.utils.map.RiverType;
+import ua.ihromant.sod.utils.map.RoadType;
 
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.util.Collections;
 import java.util.Objects;
 import java.util.zip.GZIPInputStream;
 
@@ -41,32 +38,56 @@ public class H3MParserTest {
         MapMetadata meta = testFileName("GeneratedJC");
         int size = meta.getTiles()[0].length;
         Document doc = DocumentHelper.createDocument();
-        Element root = doc.addElement("div");
-        root.addAttribute("class", "adv-map-content vertical");
-        Element top = root.addElement("div").addAttribute("class", "horizontal");
+        Element wrapper = doc.addElement("div").addAttribute("class", "adv-map-content")
+                .addAttribute("style", "width:" + (size * 32 + 320) + "px;height:" + (size * 32 + 320) + "px;");
+        Element background = wrapper.addElement("div").addAttribute("class", "vertical").addAttribute("style", "position:absolute;");
+        Element roads = wrapper.addElement("div").addAttribute("style", "position:absolute;");
+        Element rivers = wrapper.addElement("div").addAttribute("style", "position:absolute;");
+        Element top = background.addElement("div").addAttribute("class", "horizontal");
         top.addElement("img").addAttribute("src", "img/border/top_left.png");
         for (int i = 0; i < size; i++) {
             top.addElement("img").addAttribute("src", "img/border/top_" + (i % 4) + ".png");
         }
         top.addElement("img").addAttribute("src", "img/border/top_right.png");
         for (int i = 0; i < size; i++) {
-            Element row = root.addElement("div").addAttribute("class", "horizontal");
+            Element row = background.addElement("div").addAttribute("class", "horizontal");
             row.addElement("img").addAttribute("src", "img/border/left_" + (i % 4) + ".png");
             for (int j = 0; j < size; j++) {
                 MapTile tile = meta.getTiles()[0][i][j];
                 row.addElement("img")
                         .addAttribute("src", "img/tiles/" + tile.getTerrainType().toString().toLowerCase() + ".png")
                         .addAttribute("style", "width:32px;height:32px;object-position:0px "
-                                + (-32 * tile.getTerrainSprite()) + "px;object-fit:cover;" + evaluateTransform(tile.getMirroring(), 0));
+                                + (-32 * tile.getTerrainSprite()) + "px;object-fit:cover;"
+                                + evaluateTransform(tile.getMirroring(), 0));
+                if (tile.getRiverType() != RiverType.NONE) {
+                    rivers.addElement("img").addAttribute("src", "img/rivers/" + tile.getRiverType().toString().toLowerCase() + ".png")
+                            .addAttribute("style", "position:absolute;"
+                                    + "top:" + (32 * i + 32) + "px;left:" + (32 * j + 32) + "px;"
+                                    + "width:32px;height:32px;object-position:0px "
+                                    + (-32 * tile.getRiverSprite()) + "px;object-fit:cover;"
+                                    + evaluateTransform(tile.getMirroring(), 2));
+                }
+                if (tile.getRoadType() != RoadType.NONE) {
+                    roads.addElement("img").addAttribute("src", "img/roads/" + tile.getRoadType().toString().toLowerCase() + ".png")
+                            .addAttribute("style", "position:absolute;"
+                                    + "top:" + (32 * i + 48) + "px;left:" + (32 * j + 32) + "px;"
+                                    + "width:32px;height:32px;object-position:0px "
+                                    + (-32 * tile.getRoadSprite()) + "px;object-fit:cover;"
+                                    + evaluateTransform(tile.getMirroring(), 4));
+                }
             }
             row.addElement("img").addAttribute("src", "img/border/right_" + (i % 4) + ".png");
         }
-        Element bottom = root.addElement("div").addAttribute("class", "horizontal");
+        Element bottom = background.addElement("div").addAttribute("class", "horizontal");
         bottom.addElement("img").addAttribute("src", "img/border/bottom_left.png");
         for (int i = 0; i < size; i++) {
             bottom.addElement("img").addAttribute("src", "img/border/bottom_" + (i % 4) + ".png");
         }
         bottom.addElement("img").addAttribute("src", "img/border/bottom_right.png");
+
+        wrapper.addElement("div").addAttribute("class", "grid")
+                .addAttribute("style", "position:relative;width:" + (32 * size + 64) + "px;height:" + (32 * size + 64) + "px;");
+
         OutputFormat format = OutputFormat.createPrettyPrint();
         HTMLWriter writer = new HTMLWriter(System.out, format);
         writer.write(doc);
